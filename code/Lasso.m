f8bc8f4d8231ce552a28f0a6ea66cfdb7fbc2b03
Rohit Narayanan M <rohitnanu.m@gmail.com>
2021-06-26 11:07:56 +0530

    code

--- code/Lasso.m
+++ code/Lasso.m
@@ -1,2 +1,25 @@
-%lasso regressio
-%data=py.pandas.read_csv('train.csv');
+%lasso regression
+
+%,survived,age,sibsp,parch,fare,1,2,3,female,male,C,Q,S
+data=csvread('../dataset/train.csv');
+percTn=75;
+[TnSetF, TnSetL, TtSetF, TtSetL]=SplitTrainTestSet(data,percTn);
+
+%LASSO
+lasso(TnSetF,TnSetL,'Alpha',0.5)
+
+
+
+function[TnSetF, TnSetL, TtSetF, TtSetL]=SplitTrainTestSet(Data,PercTn)
+    Feature=Data(:,3:14); 
+    Species=Data(:,2);
+    TotalNumSamples=length(Species); 
+    NumTnSamp=ceil(TotalNumSamples*PercTn/100); 
+    Indx=randperm(TotalNumSamples); 
+    TnSamples=Indx(1:NumTnSamp); 
+    TtSamples=Indx(1+NumTnSamp:end); 
+    TnSetF=Feature(TnSamples,:); 
+    TnSetL=Species(TnSamples,:); 
+    TtSetF=Feature(TtSamples,:); 
+    TtSetL=Species(TtSamples,:); 
+end
