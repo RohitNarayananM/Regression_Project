57689bc66fa03c3d4ff869325785f791656e0dbd
sudi050 <sudi050@gmail.com>
2021-07-24 08:54:37 +0530

    Update Ridge.m

--- code/Ridge.m
+++ code/Ridge.m
@@ -1,25 +1,14 @@
-
 %,survived,age,sibsp,parch,fare,1,2,3,female,male,C,Q,S
 %ridge regression
 
 %,survived,age,sibsp,parch,fare,1,2,3,female,male,C,Q,S
 data=csvread('../dataset/train.csv');
+M=891;
+N=12;
 percTn=75;
 [TnSetF, TnSetL, TtSetF, TtSetL]=SplitTrainTestSet(data,percTn);
 
 %RIDGE
-<<<<<<< HEAD
-mse=100;
-for l=0.01:0.01:1
-    Bridge=ridge(TnSetL,TnSetF,l);
-    ridge_Predict=TtSetF*Bridge;
-    if mse > mean((TtSetL - ridge_Predict).^2)
-        lambda=l;
-        mse=mean((TtSetL - ridge_Predict).^2);
-        m=sum((TtSetL - ridge_Predict)/length(TtSetL));
-    end
-end
-=======
 l=1e-3;
 Bridge=ridge(TnSetL,TnSetF,l);
 ridge_Predict=TtSetF*Bridge>0.5;
@@ -28,7 +17,6 @@ mae=mean(TtSetL - ridge_Predict)
 confusionchart(logical(TtSetL),ridge_Predict);
 title("Ridge")
 acc=sum(ridge_Predict==TtSetL)/length(ridge_Predict)
->>>>>>> 2382e4765df19bbc6ed380851fb5f179cb81b333
 
 function[TnSetF, TnSetL, TtSetF, TtSetL]=SplitTrainTestSet(Data,PercTn)
     Feature=Data(:,3:14); 
@@ -42,4 +30,4 @@ function[TnSetF, TnSetL, TtSetF, TtSetL]=SplitTrainTestSet(Data,PercTn)
     TnSetL=Species(TnSamples,:); 
     TtSetF=Feature(TtSamples,:); 
     TtSetL=Species(TtSamples,:);
-end
+end% 
