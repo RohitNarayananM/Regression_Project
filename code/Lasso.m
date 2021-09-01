900fef99c13c9cabe1e4129b850134dd19b861f2
Rohit Narayanan M <rohitnanu.m@gmail.com>
2021-09-01 16:58:45 +0530

    finalize

--- code/Lasso.m
+++ code/Lasso.m
@@ -11,14 +11,9 @@ percTn=75;
 %lassoPlot(lasso_model,stats,'PlotType','CV');
 Blasso=[lasso_model(:,stats.Index1SE)];
 lasso_Predict=TtSetF*Blasso;
-
-<<<<<<< HEAD
-confusionchart(logical(TtSetL),lasso_Predict>0.5);
-=======
-confusionchart(logical(TtSetL),lasso_Predict);
+confusionchart(logical(TtSetL),(lasso_Predict>0.5));
 title("Lasso")
-acc=sum(lasso_Predict==TtSetL)/length(lasso_Predict)
->>>>>>> 2382e4765df19bbc6ed380851fb5f179cb81b333
+acc=sum((lasso_Predict>0.5)==TtSetL)/length(lasso_Predict)*100
 mse=mean((TtSetL - lasso_Predict).^2)
 mae=mean(TtSetL - lasso_Predict)
 %scatter(1:1:222, lasso_Predict)
--- code/Ridge.m
+++ code/Ridge.m
@@ -10,18 +10,6 @@ percTn=75;
 [TnSetF, TnSetL, TtSetF, TtSetL]=SplitTrainTestSet(data,percTn);
 
 %RIDGE
-<<<<<<< HEAD
-mse=100;
-for l=0.01:0.01:1
-    Bridge=ridge(TnSetL,TnSetF,l);
-    ridge_Predict=TtSetF*Bridge;
-    if mse > mean((TtSetL - ridge_Predict).^2)
-        lambda=l;
-        mse=mean((TtSetL - ridge_Predict).^2);
-        m=sum((TtSetL - ridge_Predict)/length(TtSetL));
-    end
-end
-=======
 l=1e-3;
 Bridge=ridge(TnSetL,TnSetF,l);
 ridge_Predict=TtSetF*Bridge>0.5;
@@ -30,7 +18,6 @@ mae=mean(TtSetL - ridge_Predict)
 confusionchart(logical(TtSetL),ridge_Predict);
 title("Ridge")
 acc=sum(ridge_Predict==TtSetL)/length(ridge_Predict)
->>>>>>> 2382e4765df19bbc6ed380851fb5f179cb81b333
 
 function[TnSetF, TnSetL, TtSetF, TtSetL]=SplitTrainTestSet(Data,PercTn)
     Feature=Data(:,3:14); 
